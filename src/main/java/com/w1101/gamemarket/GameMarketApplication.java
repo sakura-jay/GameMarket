0faf3a7c30740066fdfa4a7eaf71ca82772c8370
qihui <1600587398@qq.com>
2024-12-13 14:34:43 +0800

    test

---
+++ src/main/java/com/w1101/gamemarket/GameMarketApplication.java
@@ -0,0 +1,13 @@
+package com.w1101.gamemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/w1101/gamemarket/GameMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w1101.gamemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
